091fa0012dfaeece9388806ea1a1fde703f758f0
andy-phu <andyphu7601@gmail.com>
2023-09-06 19:09:16 -0700

    another test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () =>{
                 {/*Overlay */}
                 <div className='absolute w-full h-full text-gray-200 bg-black/40 flex flex-col justify-center items-center h-screen ' >
                     <h2 className=' text-gray-200 px-4 text-4xl sm:text-5xl md:text-6xl lg:text-6xl font-bold tracking-widest'>IRVINE FINTECH ASSOCIATION</h2>
-                    <h2 className=' text-gray-200 px-4 text-2xl sm:text-3xl md:text-4xl lg:text-3xl tracking-widest'>University of California, Irvine</h2>
+                    <h2 className=' text-gray-200 px-4 text-2xl sm:text-3xl md:text-4xl lg:text-3xl tracking-widest'>University of California, terst test sets Irvine</h2>
                     <h2 className=' text-gray-200 px-4 text-2xl sm:text-3xl md:text-4xl lg:text-3xl tracking-widest'>Est. 2023</h2>
 
                 </div>
@@ -18,15 +18,18 @@ const Hero = () =>{
                 <div className='absolute bottom-0 left-1/2 transform -translate-x-1/2 '>
                     <RiArrowDropDownLine className='w-20 h-16 z-10 text-white' />
                 </div>
+                
             </div>
-
-            {/*description */}
-            
-        </div>
 
             
         
+            
+        </div>
+
+      
     )
 }
 
 export default Hero
+
+//testign more comments to see whats up
