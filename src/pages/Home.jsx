86be24154c6822ad9d173a95749839e5c635fddc
andy-phu <andyphu7601@gmail.com>
2023-09-08 23:11:01 -0700

    netlify fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Mission from '../components/Mission'
 import Values from '../components/Values'
-import Seperator from '../components/Seperator'
 import Affiliations from '../components/Affiliations'
 import Footer from '../components/Footer'
 
