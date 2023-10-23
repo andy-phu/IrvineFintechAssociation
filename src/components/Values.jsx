5fa00fd84f647f892d8af023a1f12891d7bf4b56
andy-phu <andyphu7601@gmail.com>
2023-10-23 00:56:48 -0700

    before i mess around with the size of the images

--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-import man from '../images/vecteezy_worker-climbs-the-bar-graph-with-the-green-arrow-up-career_16779656_364.png';
-import pig from '../images/pngwing.com.png';
-import networking from '../images/pngwing.com-2.png';
+// import man from '../images/vecteezy_worker-climbs-the-bar-graph-with-the-green-arrow-up-career_16779656_364.png';
+// import pig from '../images/pngwing.com.png';
+// import networking from '../images/pngwing.com-2.png';
+import AIF from '../images/ifa-AIF.jpeg';
+import firstMeeting from '../images/ifa-firstMeeting.png';
+import secondMeeting from '../images/ifa-secondMeeting.png';
 
 const Values = () => {
     return (
@@ -13,7 +16,7 @@ const Values = () => {
                 <div className="w-2/3 h-2/3 lg:w-1/4 lg:h-1/4 text-center flex flex-col justify-center items-center bg-green-700 bg-opacity-78">
                     <h1 className='text-opacity-78 text-4xl font-bold text-gray-200 pb-2'>CAREER</h1>
                     <div className='flex justify-center py-8 mb-2'>
-                        <img className='w-8/9 h-8/9 lg:w-48 lg:h-48 object-cover' src={man} alt="manWalking"/>
+                        <img className='w-8/9 h-8/9 lg:w-48 lg:h-48 object-cover' src={AIF} alt="anteater involvement fair"/>
                     </div>
                     <p className='text-gray-200 text-lg'>
                         Connect students with career opportunities, such as internships and full-time positions in the dynamic FinTech industry.
@@ -23,7 +26,7 @@ const Values = () => {
                 <div className="w-3/4 h-3/4 lg:w-1/4 lg:h-1/4  border-opacity-78 text-center flex flex-col justify-center items-center bg-green-700 bg-opacity-78 ">
                     <h1 className='text-gray-200 text-opacity-78 text-4xl font-bold'>EDUCATION</h1>
                     <div className='flex justify-center py-4'>
-                        <img className='w-56 h-56 object-cover' src={pig} alt="pig"/>
+                        <img className='w-56 h-56 object-cover' src={firstMeeting} alt="first meeting"/>
                     </div>
                     <p className='text-gray-200 text-lg'>
                         Equip students with the proper industry knowledge, skills and current news/updates about FinTech.
@@ -32,7 +35,7 @@ const Values = () => {
                 <div className="w-3/4 h-3/4 lg:w-1/4 lg:h-1/4  text-center flex flex-col justify-center items-center bg-green-700 bg-opacity-78 ">
                     <h1 className='text-gray-200  text-opacity-78 text-4xl font-bold pb-2'>NETWORKING</h1>
                     <div className='flex justify-center py-12 mt-2'>
-                        <img className='w-full h-40 object-cover' src={networking} alt="networking"/>
+                        <img className='w-full h-40 object-cover' src={secondMeeting} alt="second meeting"/>
                     </div>
                     <p className='text-gray-200 text-lg'>
                         Build a community through networking opportunities with alumni, professors, and professionals.
