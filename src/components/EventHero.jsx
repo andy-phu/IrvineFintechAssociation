a38db292f5ebd399b2279040780a2946e5b22dd8
andy-phu <andyphu7601@gmail.com>
2023-10-29 23:49:43 -0700

    value pictures changed

--- src/components/EventHero.jsx
+++ src/components/EventHero.jsx
@@ -6,13 +6,13 @@ import {RiArrowDropDownLine} from "react-icons/ri"
 const EventHero = () =>{
     return(
         <div id='hero'  className='mx-auto h-screen'>
-            <div className='h-full w-full h-screen relative' >
+            <div className='w-full h-screen relative' >
                 {/*Overlay */}
                 <div className='absolute w-full h-full text-gray-200 bg-black/40 flex flex-col justify-center items-center h-screen ' >
                     <h2 className=' text-gray-200 px-4 text-4xl sm:text-5xl md:text-6xl lg:text-6xl font-bold tracking-widest'>EVENTS</h2>
 
                 </div>
-                <img className='h-screen w-full w-screen object-cover' src={heroIFA} alt="hero"/>
+                <img className='h-screen  w-screen object-cover' src={heroIFA} alt="hero"/>
                 <div className='absolute bottom-0 left-1/2 transform -translate-x-1/2 '>
                     <RiArrowDropDownLine className='w-20 h-16 z-10 text-white' />
                 </div>
--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -2,7 +2,7 @@ import React from "react";
 // import man from '../images/vecteezy_worker-climbs-the-bar-graph-with-the-green-arrow-up-career_16779656_364.png';
 // import pig from '../images/pngwing.com.png';
 // import networking from '../images/pngwing.com-2.png';
-import AIF from '../images/ifa-AIF.jpeg';
+import cha from '../images/ifa-chaTea.png';
 import firstMeeting from '../images/ifa-firstMeeting.png';
 import secondMeeting from '../images/ifa-secondMeeting.png';
 
@@ -12,34 +12,34 @@ const Values = () => {
             <h1 className='text-6xl font-extrabold mt-8 mb-12'>
                 OUR GOALS
             </h1>
-            <div className='flex justify-center items-center text-center lg:space-x-12 space-y-10 lg:space-y-0 flex-col lg:flex-row '>
-                <div className="w-2/3 h-2/3 lg:w-1/4 lg:h-1/4 text-center flex flex-col justify-center items-center bg-green-700 bg-opacity-78">
-                    <h1 className='text-opacity-78 text-4xl font-bold text-gray-200 pb-2'>CAREER</h1>
-                    <div className='flex justify-center py-8 mb-2'>
-                        <img className='w-8/9 h-8/9 lg:w-48 lg:h-48 object-cover' src={AIF} alt="anteater involvement fair"/>
+            <div className='flex justify-center items-center text-center lg:space-x-12 space-y-10 lg:space-y-0 flex-col lg:flex-row'>
+                <div className="w-3/4 h-3/4 lg:w-1/4 lg:h-1/4 text-center flex flex-col justify-center items-center bg-green-700 bg-opacity-78">
+                    <div className='text-opacity-78 text-4xl font-bold text-gray-200 mb-2'>CAREER</div>
+                    <div className='flex justify-center mb-2 overflow-hidden'>
+                        <img className='w-full h-full object-cover' src={secondMeeting} alt="second meeting"/>
                     </div>
-                    <p className='text-gray-200 text-lg'>
+                    <div className='text-gray-200 text-lg'>
                         Connect students with career opportunities, such as internships and full-time positions in the dynamic FinTech industry.
-                    </p>
+                    </div>
 
                 </div>
-                <div className="w-3/4 h-3/4 lg:w-1/4 lg:h-1/4  border-opacity-78 text-center flex flex-col justify-center items-center bg-green-700 bg-opacity-78 ">
-                    <h1 className='text-gray-200 text-opacity-78 text-4xl font-bold'>EDUCATION</h1>
-                    <div className='flex justify-center py-4'>
-                        <img className='w-56 h-56 object-cover' src={firstMeeting} alt="first meeting"/>
+                <div className="w-3/4 h-3/4 lg:w-1/4 lg:h-1/4 text-center flex flex-col justify-center items-center bg-green-700 bg-opacity-78">
+                    <div className='text-opacity-78 text-4xl font-bold text-gray-200 mb-2'>EDUCATION</div>
+                    <div className='flex justify-center mb-2 overflow-hidden'>
+                        <img className='w-full h-full object-cover' src={firstMeeting} alt="first meeting"/>
                     </div>
-                    <p className='text-gray-200 text-lg'>
+                    <div className='text-gray-200 text-lg'>
                         Equip students with the proper industry knowledge, skills and current news/updates about FinTech.
-                    </p>
+                    </div>
                 </div>
                 <div className="w-3/4 h-3/4 lg:w-1/4 lg:h-1/4  text-center flex flex-col justify-center items-center bg-green-700 bg-opacity-78 ">
-                    <h1 className='text-gray-200  text-opacity-78 text-4xl font-bold pb-2'>NETWORKING</h1>
-                    <div className='flex justify-center py-12 mt-2'>
-                        <img className='w-full h-40 object-cover' src={secondMeeting} alt="second meeting"/>
+                    <div className='text-opacity-78 text-4xl font-bold text-gray-200 mb-2'>NETWORKING</div>
+                    <div className='flex justify-center mb-2 overflow-hidden'>
+                        <img className='w-full h-full object-cover' src={cha} alt="cha for tea fundraiser"/>
                     </div>
-                    <p className='text-gray-200 text-lg'>
+                    <div className='text-gray-200 text-lg'>
                         Build a community through networking opportunities with alumni, professors, and professionals.
-                    </p>
+                    </div>
                 </div>
             </div>
 
