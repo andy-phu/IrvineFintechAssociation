41d855e8c4ac96168f08ce5556226a0cd71c837f
andy-phu <andyphu7601@gmail.com>
2024-03-25 22:08:29 -0700

    home partially changed

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -5,9 +5,9 @@ import logoWithDescription from '../images/logo-with-text-transparent.png'
 
 const Mission = () => {
     return (
-        <div className=' text-gray-900 h-1/2 mt-10 mb-14'>
+        <div className=' text-gray-900 h-screen mt-10 mb-14'>
             
-            <div className='flex justify-center items-center text-center w-screen h-1/2'>
+            <div className='flex justify-center items-center text-center w-screen h-screen'>
                 <div className='w-screen lg:w-4/5 p-6'>
                     <p className='flex justify-center text-5xl lg:text-6xl text-green-700 text-opacity-78 font-extrabold tracking-widest py-2'>OUR MISSION</p>
                     <p className='flex justify-center text-xl lg:text-2xl tracking-widest font-bold py-2'>Shaping Tomorrow’s Finance Through Tech Exploration</p>
